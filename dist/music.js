e44d7b7da4c4b54fa2f276dd3d9e726bc50b42ae
Jrafina <1795857787@qq.com>
2025-10-25 14:08:54 +0800

    Site updated: 2025-10-25 14:08:54

--- dist/music.js
+++ dist/music.js
@@ -4,58 +4,76 @@ const ap = new APlayer({
   // autoplay: true,
   audio: [
     {
-      name: '溯（钢琴版）',
-      artist: '柳轻颂',
-      url: 'http://music.163.com/song/media/outer/url?id=1433584979.mp3',
-      cover: 'http://p1.music.126.net/qX7knUIlpmbJ34UBzCCS6w==/109951164802366812.jpg?param=130y130',
-    },
-    {
-      name: "夢灯籠",
-      artist: 'RADWIMPS',
-      url: '/images/夢灯籠.mp3',
-      cover: 'http://p2.music.126.net/sSxbRt9RpC6s_MaewyDJfA==/18597139672292692.jpg?param=130y130',
-    },
-    {
-      name: '前前前世 (movie ver.)',
-      artist: 'RADWIMPS',
-      url: '/images/前前前世-_movie-ver._-RADWIMPS.mp3',
-      cover: 'http://p2.music.126.net/sSxbRt9RpC6s_MaewyDJfA==/18597139672292692.jpg?param=130y130',
-    },
-    {
-      name: 'Supermarket Flowers',
-      artist: 'Ed Sheeran',
-      url: '/images/Supermarket Flowers.mp3',
-      cover: 'http://p2.music.126.net/99_i681E6ZE74t_xue6PUA==/109951166151204092.jpg?param=130y130',
-    },
-    {
-      name: 'スパークル（火花）——《你的名字。》插曲',
-      artist: 'ゆう十',
-      url: 'http://music.163.com/song/media/outer/url?id=1984045303.mp3',
-      cover: 'http://p2.music.126.net/SiTInUUumsPdSdndv1MCyw==/109951167903888878.jpg?param=130y130',
-    },
-    {
-      name: 'euphoria',
-      artist: ' ',
-      url: 'http://music.163.com/song/media/outer/url?id=1814327255.mp3',
-      cover: '/images/susan1.jpg',
-    },
-    {
-      name: 'Friend',
-      artist: 'Kozoro',
-      url: 'http://music.163.com/song/media/outer/url?id=33004707.mp3',
-      cover: 'http://p2.music.126.net/FR7zKYB8ujNRoWQuGCHbGg==/109951163288692527.jpg?param=130y130',
-    },
-    {
-      name: 'One Last Time',
-      artist: '廖泽蓝',
-      url: 'http://music.163.com/song/media/outer/url?id=1870680152.mp3',
-      cover: 'http://p1.music.126.net/Y_Io_ln1-ujiL9eFdyQvmQ==/109951166291345875.jpg?param=130y130',
-    },
-    {
       name: '起风了',
-      artist: '买',
-      url: './music/起风了.mp3',
-      cover: './img/background.png',
+      artist: '买辣椒也用券',
+      url: 'https://lw-sycdn.kuwo.cn/7350192ff54952967b3101ba9338005f/68fc63cd/resource/30106/trackmedia/M500000yLI8y2RJRq3.mp3',
+      cover: 'http://img4.kuwo.cn/star/albumcover/500/15/82/150990423.jpg',
     },
+    {
+      name: '把回忆拼好给你',
+      artist: 'cici_',
+      url: 'https://er-sycdn.kuwo.cn/a4727f4476dd630b60e8aae4c97b9b3a/68fc5166/resource/30106/trackmedia/M500002trXIw2XBExG.mp3?bitrate$128&from=vip',
+      cover: 'http://img4.kuwo.cn/star/albumcover/500/7/22/3737107551.jpg',
+    },
+    {
+      name: 'Wonderful U',
+      artist: 'AGA',
+      url: 'https://lv-sycdn.kuwo.cn/850ffa53a80aea265c2e9c0470ff82e3/68fc651b/resource/30106/trackmedia/M5000040GeTC10zC7y.mp3?bitrate$128&from=vip',
+      cover: 'http://img3.kuwo.cn/star/starheads/500/8/92/3814852462.jpg',
+    },
+    {
+      name: '勇气',
+      artist: '棉子',
+      url: 'https://lv-sycdn.kuwo.cn/ab727322343d49bca00a07e9eeeb1b85/68fc65ee/resource/30106/trackmedia/M500001YtzzT0gzDFH.mp3?bitrate$128&from=vip',
+      cover: 'http://img4.kuwo.cn/star/albumcover/500/24/91/3155287882.jpg',
+    },
+    {
+      name: '冬眠',
+      artist: '司南',
+      url: 'https://lw-sycdn.kuwo.cn/6df2739e02ab20ca459263b04429283b/68fc58d8/resource/30106/trackmedia/M500001UYqL21cY2wv.mp3?bitrate$128&from=vip',
+      cover: 'http://img3.kuwo.cn/star/albumcover/500/60/22/2445771895.jpg',
+    },
+    {
+      name: '搁浅',
+      artist: '周杰伦',
+      url: 'https://lx-sycdn.kuwo.cn/844e6d62326bade3274ed712352812e6/68fc522d/resource/n3/60/48/2517243709.mp3?bitrate$128&from=vip',
+      cover: 'http://img2.kuwo.cn/star/albumcover/500/30/97/4276557883.jpg',
+    },
+    {
+      name: '珊瑚海',
+      artist: '周杰伦&Lara梁心颐',
+      url: 'https://er-sycdn.kuwo.cn/ead5be288cf6b5708f399d0b1eb166e7/68fc54c1/resource/30106/trackmedia/M500001K0AjL2huSxx.mp3?bitrate$128&from=vip',
+      cover: 'http://img3.kuwo.cn/star/albumcover/500/32/88/1567952196.jpg',
+    },
+    {
+      name: '晴天',
+      artist: '周杰伦',
+      url: 'https://er-sycdn.kuwo.cn/1dbd30d51834e7cbdef1901f9cdecbcf/68fc5172/resource/30106/trackmedia/M500000bYDlc2XxKLs.mp3?bitrate$128&from=vip',
+      cover: 'http://img2.kuwo.cn/star/albumcover/500/s3s94/93/211513640.jpg',
+    },
+    {
+      name: '七里香',
+      artist: '周杰伦',
+      url: 'https://lv-sycdn.kuwo.cn/1f05724e18a916c18824c04abb919e3b/68fc5562/resource/30106/trackmedia/M500004Z8Ihr0JIu5s.mp3?bitrate$128&from=vip',
+      cover: 'http://img2.kuwo.cn/star/albumcover/500/30/97/4276557883.jpg',
+    },
+    {
+      name: '她说',
+      artist: '林俊杰',
+      url: 'https://er-sycdn.kuwo.cn/ef234cc5f7896f3d20d6805c884accbc/68fc5405/resource/30106/trackmedia/M500003qIQp80hyOQC.mp3?bitrate$128&from=vip',
+      cover: 'http://img1.kuwo.cn/star/albumcover/500/21/12/4189818596.jpg',
+    },
+    {
+      name: '不为谁而做的歌',
+      artist: '林俊杰',
+      url: 'https://lv-sycdn.kuwo.cn/3443c31d5f7308fe3d623e5e780833a5/68fc538c/resource/30106/trackmedia/M500000VDpsz1LmjL4.mp3?bitrate$128&from=vip',
+      cover: 'http://img4.kuwo.cn/star/albumcover/500/62/23/789477357.jpg',
+    },
+    {
+      name: '泡沫',
+      artist: '邓紫棋',
+      url: 'https://er-sycdn.kuwo.cn/4d7f969bef587b968693ef60840e9f32/68fc53a2/resource/30106/trackmedia/M500003CL1jU1Wgcit.mp3?bitrate$128&from=vip',
+      cover: 'http://img1.kuwo.cn/star/albumcover/500/24/35/4281428590.jpg',
+    }
   ]
 });
